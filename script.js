e80d400240284fb3e8a800a8c1e3397bf197b947
Csilla Toth <csillaout@gmail.com>
2024-10-18 14:48:26 +0100

    update

--- script.js
+++ script.js
@@ -6,4 +6,21 @@
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getFullYear
 
-console.log("Hello, World!");
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the image and button elements
+  const image = document.getElementById("profile-image");
+  const button = document.querySelector(".change");
+
+  // Set the alternative image source
+  const newImageSrc = "/src/images/uniqornish.jpeg";
+
+  // Add an event listener to the button
+  button.addEventListener("click", () => {
+    // Change the image source
+    if (image.src.includes("csillatoth.png")) {
+      image.src = newImageSrc;
+    } else {
+      image.src = "/src/images/csillatoth.png"; // Revert to the original image if clicked again
+    }
+  });
+});
